a48184b4742448953bd60e5afd2cc10af0ee4539
Juan Orjuela <orjuelajuans@gmail.com>
2018-04-04 09:30:11 -0500

    Avances nuevos

--- ejercicio-1/2-JS-Basics/script.js
+++ ejercicio-1/2-JS-Basics/script.js
@@ -1,15 +1,18 @@
 //Ejercicio programación 2
 var years = [1960, 1945, 1990, 2008, 1919, 2010];
-var ages = [];
 
-for (var i = 0; i < years.length; i++ ) {
-    var age = 2018 - years[i];
-    ages.push(age);
-}
-for ( i = 0 ; i < ages.length ; i++) {
-    if (ages[i] < 18 ) {
-        console.log('Perrito no es mayor de edad, pues tiene '+ages[i]+' años');
-    } else {
-        console.log('Perrito ya está viejo, tiene '+ages[i]+' años');
+
+
+function printFullAge(array){
+    for (var i = 0; i < array.length; i++ ) {
+        var age = 2018 - array[i];
+        ages.push(age);
     }
+    for ( i = 0 ; i < ages.length ; i++) {
+        if (ages[i] >= 18 ) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
 }
