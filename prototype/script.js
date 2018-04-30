78761ddf03e7ced98aa856d49d95318cccdb4a79
Juan Orjuela <orjuelajuans@gmail.com>
2018-04-30 18:13:02 -0500

    Update script.js

--- prototype/script.js
+++ prototype/script.js
@@ -1,1 +1,20 @@
+//Function constructor
 
+var juan = {
+  nombre: 'Juan',
+  fechaNacimiento: 1990,
+  trabajo: 'Profesor'
+};
+
+var Persona = function(nombre, fechaNacimiento, trabajo) {
+  this.nombre = nombre;
+  this.fechaNacimiento = fechaNacimiento;
+  this.trabajo = trabajo;
+  this.calcularEdad = function() {
+    console.log(2018 - fechaNacimiento);
+  };
+}
+
+var alicia = new Persona('Alicia', 1982, 'Musico');
+
+alicia.calcularEdad();
